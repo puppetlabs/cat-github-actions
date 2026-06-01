c63b54a274a9d6b5c3728ea8ba00337d6f6e24f3
bronachfalls <bronach.falls@perforce.com>
2026-06-01 14:48:01 +0100

    testing script

--- scripts/token_expiry.rb
+++ scripts/token_expiry.rb
@@ -3,7 +3,7 @@ require "json"
 require "uri"
 
 HOST = "https://forgeapi.puppet.com"
-TOKEN = ENV.fetch("API_TOKEN")
+TOKEN = ENV.fetch("API_KEY")
 
 uri = URI("#{HOST}/private/keys")
 http = Net::HTTP.new(uri.host, uri.port)
@@ -11,11 +11,14 @@ http.use_ssl = true
 
 request = Net::HTTP::Get.new(uri)
 request["Authorization"] = "Bearer #{TOKEN}"
+puts "Using token: #{TOKEN[0..5]}..."
 
 response = http.request(request)
 puts response["location"]
+puts response.code
+puts response.body
 raise "Request failed: #{response.code}" unless response.code == "200"
-puts("hellloooo 2")
+
 puts response["location"]
 
 body = JSON.parse(response.body)
